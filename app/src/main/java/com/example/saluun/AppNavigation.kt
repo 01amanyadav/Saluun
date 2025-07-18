ee15c06a4fccb6c2983d9264717e15de7d2012c5
01amanyadav <08amanyadav@gmail.com>
2025-07-18 09:12:32 +0530

    Adding NavHost

--- app/src/main/java/com/example/saluun/AppNavigation.kt
+++ app/src/main/java/com/example/saluun/AppNavigation.kt
@@ -1,9 +1,16 @@
 package com.example.saluun
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
+@Composable
 fun AppNavigation(){
     val navController  = rememberNavController()
+    NavHost(navController = navController, startDestination = "login", builder = {
+
+    })
+    }
     
 
 }
